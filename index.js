cf17d2b47d51a5ebe33e7838d365959ff5c308db
Alvaro Calace <alvaro.calace@sovanta.com>
2018-05-08 12:41:51 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const program = require("commander");
+
+function initProgram() {
+    
+}
+
+function main() {
+    initProgram();
+
+    try {
+
+    } catch(error) {
+        console.error(error);
+    }
+}
+
+main();
